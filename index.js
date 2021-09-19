979062f55b7a31f4cebb4ca1b84bf9f5e2ee9778
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-09-19 19:33:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var readlineSync = require('readline-sync');
+
+score = 0;
+
+var userName = readlineSync.question('What is your name? ');
+
+console.log("welcome! to this Quiz", userName);
+
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer === answer) {
+    console.log("You are right");
+    score = score + 1;
+  } else {
+    console.log("You are wrong");
+    score = score - 1;
+  }
+  console.log("Your score is: " + score)
+  console.log("---------")
+}
+
+console.log("Start your answers with first latter capital")
+var asked =[{
+  question: "Do you live in India? ",
+  answer: "Yes"
+}, {   
+question: "What is a Capital of India? ",
+answer: "Delhi"
+},{
+  question: "which year India got Independance? ",
+answer: "1947"
+},{
+  question: "Who is current prime minister of India? ",
+answer: "Narendra Modi"
+},{
+  question: "Is Pandit Jawaharlal Nehru was first prime minister of India? ",
+answer: "Yes"
+}
+
+];
+
+for(i=0; i<asked.length; i++) {
+play(asked[i].question,asked[i].answer);
+}
+
+console.log("Wow! Your total score is: " + score)
+
+var highscores =[{
+  name: "Tejas", 
+  score: "5"
+},{
+  name: "Mayur",
+  score: "4"
+}]
+
+for(var i=0; i<highscores.length; i++) 
+console.log(highscores[i].name , "total score was: " + highscores[i].score);  
+
+console.log("Gave me your Highscore on Whatsapp so I will update highscore list ")
+
+
+
+
+
+
+
+
+
